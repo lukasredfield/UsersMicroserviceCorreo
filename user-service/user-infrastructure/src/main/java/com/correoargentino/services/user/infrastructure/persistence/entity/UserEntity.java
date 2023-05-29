86d125cce89afd31b2fbf9953c1a96199b660857
Claudio Amoedo <claudioamoedo@gmail.com>
2023-05-29 09:25:50 -0300

    Initial commit

---
+++ user-service/user-infrastructure/src/main/java/com/correoargentino/services/user/infrastructure/persistence/entity/UserEntity.java
@@ -0,0 +1,25 @@
+package com.correoargentino.services.user.infrastructure.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UserEntity {
+  @Id
+  private UUID id;
+
+  @Column(name = "created_at", nullable = false)
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+}
