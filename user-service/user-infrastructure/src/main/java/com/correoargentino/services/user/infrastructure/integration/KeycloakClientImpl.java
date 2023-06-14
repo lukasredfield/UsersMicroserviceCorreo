223111a0f9903732b365389a1377f67dc86ca88a
Claudio Amoedo <claudioamoedo@gmail.com>
2023-06-14 09:41:39 -0300

    Revert "Usuario Keycloak creado (provisorio)"
    This reverts commit 235927ccd0c1080518b3549d32d2d4157cf1fd90.

--- user-service/user-infrastructure/src/main/java/com/correoargentino/services/user/infrastructure/integration/KeycloakClientImpl.java
+++ user-service/user-infrastructure/src/main/java/com/correoargentino/services/user/infrastructure/integration/KeycloakClientImpl.java
@@ -41,9 +41,7 @@ public class KeycloakClientImpl implements KeycloakClient {
                 .realm(realm)
                 .clientId(clientId)
                 .clientSecret(clientSecret)
-                .grantType(OAuth2Constants.PASSWORD)
-                .username("lucas")
-                .password("admin")
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
                 .build();
     }
 
@@ -58,15 +56,13 @@ public class KeycloakClientImpl implements KeycloakClient {
             RealmResource realmResource = keycloakClient.realm(realm);
             UsersResource usersResource = realmResource.users();
 
-            Response response = usersResource.create(userRepresentation);
-
-            if (response.getStatus() == HttpStatus.CREATED.value()) {
-                log.info("Usuario creado en Keycloak: {}", userKeycloak.getUserName());
-            } else {
-                log.error("Error al crear el usuario en Keycloak: {}", response.getStatusInfo().getReasonPhrase());
+            try (Response response = usersResource.create(userRepresentation)) {
+                if (response.getStatus() == HttpStatus.CREATED.value()) {
+                    log.info("Usuario creado en Keycloak: {}", userKeycloak.getUserName());
+                } else {
+                    log.error("Error al crear el usuario en Keycloak: {}", response.getStatusInfo().getReasonPhrase());
+                }
             }
-
-            response.close();
         } catch (Exception e) {
             log.error("Excepción al crear el usuario en Keycloak", e);
         }
@@ -82,7 +78,6 @@ public class KeycloakClientImpl implements KeycloakClient {
         return userKeycloak;
     }
 }
-
 
 
 
