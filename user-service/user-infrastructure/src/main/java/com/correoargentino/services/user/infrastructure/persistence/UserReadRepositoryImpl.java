0acda87778f23183ea850b140617ca1bcdbf6037
lukasredfield <lukasredfield@gmail.com>
2023-06-16 13:22:53 -0300

    user keycloak in (process 2)

--- user-service/user-infrastructure/src/main/java/com/correoargentino/services/user/infrastructure/persistence/UserReadRepositoryImpl.java
+++ user-service/user-infrastructure/src/main/java/com/correoargentino/services/user/infrastructure/persistence/UserReadRepositoryImpl.java
@@ -11,7 +11,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-
 @Service
 @RequiredArgsConstructor
 public class UserReadRepositoryImpl implements UserReadRepository {
@@ -22,6 +21,7 @@ public class UserReadRepositoryImpl implements UserReadRepository {
     try {
       var sql = "SELECT * FROM users WHERE id = ?";
       var result = jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
+        UUID userId = UUID.fromString(rs.getString("id"));
         String firstName = rs.getString("first_name");
         String lastName = rs.getString("last_name");
         String emailAddress = rs.getString("email_address");
@@ -30,8 +30,8 @@ public class UserReadRepositoryImpl implements UserReadRepository {
         LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
         LocalDateTime updatedAt = rs.getTimestamp("updated_at").toLocalDateTime();
 
-        return new User(id, firstName, lastName,
-            emailAddress, phoneNumber, preferences, createdAt, updatedAt);
+        return new User(userId, firstName, lastName,
+                emailAddress, phoneNumber, preferences, createdAt, updatedAt);
       }, id);
 
       return Optional.ofNullable(result);
@@ -39,10 +39,6 @@ public class UserReadRepositoryImpl implements UserReadRepository {
       return Optional.empty();
     }
   }
+
+
 }
-
-
-
-
-
-
